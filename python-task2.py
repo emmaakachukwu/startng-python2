4c9f07831d87c9a0eb2af38317b6618f4ccb19b9
emmanuel akachukwu <emmanuelakachukwu1@gmail.com>
2020-04-24 21:32:32 +0100

    first commit

--- python-task2.py
+++ python-task2.py
@@ -5,7 +5,6 @@ last_name = input("Enter your last name: ")
 email = input("Enter your email address: ")
 
 password = first_name[:2] + last_name[-2:] + str(random.randint(10000, 99999))
-# password = first_name[0:2]
 confirm_password = input(f"Here is your new password: {password}. Confirm if you are satisfied with it. Press 'Y' for yes or any other key for no: ")
 
 if confirm_password.upper() != "Y":
@@ -15,7 +14,6 @@ if confirm_password.upper() != "Y":
             print("Password must be greater than seven characters")
             continue
         else:
-            #print out user details
             break
 
 user_data = {
